3ec9259d673e80fa3ffa1ab1459598ff6168ff90
Bill Zdon <williamzdon@gmail.com>
2023-10-02 12:36:14 -1000

    first tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,15 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'node',
-};
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  roots: [
+    "src"
+  ],
+  transformIgnorePatterns: [
+		"node_modules/(?!(@buf)/)"
+	],
+}

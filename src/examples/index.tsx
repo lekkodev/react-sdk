59493485ec02d1d2ab2816feedbd4227cd3f919a
Bill Zdon <williamzdon@gmail.com>
2023-10-18 04:45:07 +0700

    fix index

--- src/examples/index.tsx
+++ src/examples/index.tsx
@@ -2,13 +2,10 @@ import React from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App"
 
-// Get a reference to the root element
 const rootElement = document.getElementById("root")
 
-if (!rootElement) throw new Error("no root")
+if (rootElement === null) throw new Error("no root")
 
-// Create a root
 const root = createRoot(rootElement)
 
-// Render your component within the root
 root.render(<App />)

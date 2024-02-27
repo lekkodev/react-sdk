b16938756842aa681ccad5bf9f82e4a9b83f7563
David Kang <david@lekko.com>
2024-02-27 10:27:56 -0800

    Automatically load Vite env vars

--- src/utils/envHelpers.ts
+++ src/utils/envHelpers.ts
@@ -8,6 +8,11 @@ export function getEnvironmentVariable(key: EnvVariable) {
   let envValue
 
   try {
+    // @ts-expect-error Checking for Vite via import.meta.env
+    envValue = import.meta.env[`VITE_${key}`]
+  } catch (error) {}
+
+  try {
     envValue = process.env[`REACT_APP_${key}`]
   } catch (error) {}
 
